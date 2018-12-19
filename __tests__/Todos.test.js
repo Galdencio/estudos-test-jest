15147e5e5e9ae224818c136ef235a8c4e228b89f
Galdencio <galdencio@fourup.com.br>
2018-12-19 13:14:46 -0200

    Testes dos componentes App, Counter e Todos

---
+++ __tests__/Todos.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Todos from '../src/Todos';
+import configureStore from 'redux-mock-store';
+const mockStore = configureStore();
+
+const initialState = {
+    todos: [
+        { id: 0, text: 'Fazer café' },
+        { id: 1, text: 'Acessar o slack' },
+        { id: 2, text: 'Ver RocketLive' },
+    ],
+};
+
+const store = mockStore(initialState);
+
+describe('Testing Todos Component', () => {
+    it('should render correctly', () => {
+        const wrapper = shallow(
+            <Todos />,
+            { context: { store } }
+        )
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
